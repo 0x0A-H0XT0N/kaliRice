c407faa723d39a813dbc936716dc880dfd4384b7
H0XT0N <97365566+0x0A-H0XT0N@users.noreply.github.com>
2022-01-26 21:10:35 -0300

    removed useless files

--- .config/guake/export_guake_settings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# export guake settings (guake -> external file) using dconf
-# made based in this issue thread: https://github.com/Guake/guake/issues/292
-
-dconf dump /apps/guake/ > ~/.config/guake/guake_settings
-echo "settings exported to ~/.config/guake/guake_settings"
